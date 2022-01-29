dfa61c5c586c71d228355391f69f590c42d88046
philipp <philipp.wienes@einfach.es>
2022-01-30 00:22:24 +0100

    asd

--- assets/main.js
+++ assets/main.js
@@ -1,26 +1,6 @@
-/*let rows = document.querySelectorAll("h1 div");
 
-totalDuration = 4800;
-var singleDuration = totalDuration / rows.length;;
-gsap.timeline({
-  scrollTrigger: {
-    trigger: "h1",
-    pinSpacing: true,
-    pin:true,
-    start: "top top",
-    end:'+=' + totalDuration,
-  }
-})*/
 
 let rows = document.querySelectorAll("h1 div");
-
-
-
-/*var totalDuration = 5000;
-var singleDuration = totalDuration / headlines.length;
-   
-*/     
-
 
 rows.forEach(function (row,i) {
 
@@ -33,8 +13,8 @@ rows.forEach(function (row,i) {
       end: "+=100%",
     }
   })
-  .fromTo(row, {fontSize:'3200%',opacity:0, y: 0}, {fontSize:'1200%', opacity:1, y:0})
-  //.fromTo(row, {fontSize:'1200%'}, {fontSize:'60%', xPercent: - 40 + i*10}, '+=1')
+  .fromTo(row, {fontSize:'3200%',opacity:1, y: 0}, {fontSize:'1200%', opacity:1, y:0})
+  .fromTo(row, {fontSize:'1200%'}, {fontSize:'60%', xPercent: - 40 + i*10}, '+=1')
 });
 
 rows.forEach(function (row,i) {

e8caa908c0b1201ebe1645d657b3d03ffd837658
philipp <philipp.wienes@einfach.es>
2022-01-30 00:29:40 +0100

    bn

--- assets/main.js
+++ assets/main.js
@@ -13,8 +13,8 @@ rows.forEach(function (row,i) {
       end: "+=100%",
     }
   })
-  .fromTo(row, {fontSize:'3200%',opacity:0, y: 0}, {fontSize:'1200%', opacity:1, y:0})
-  .to(row, {fontSize:'60%', xPercent: - 40 + i*10}, '+=1')
+  .fromTo(row, {fontSize:'6200%',opacity:0, y: 0}, {fontSize:'1200%', opacity:1, y:0})
+  .to(row, {fontSize:'60%', xPercent: - 40 + i*10})
 });
 
 rows.forEach(function (row,i) {

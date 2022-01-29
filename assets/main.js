fd29405310067751654654bb3886e453664b9c41
philipp <philipp.wienes@einfach.es>
2022-01-30 00:47:06 +0100

    y

--- assets/main.js
+++ assets/main.js
@@ -9,24 +9,12 @@ rows.forEach(function (row,i) {
       trigger: row,
       scrub: .9,
       markers:true,
+      pinSpacing: false,
+      pin: true,
       start: 'top top',
       end: "+=100%",
     }
   })
-  .fromTo(row, {fontSize:'6200%',opacity:0, y: 0}, {fontSize:'1200%', opacity:1, y:0})
-  .to(row, {fontSize:'60%', xPercent: - 40 + i*10})
+  .fromTo(row, {rotateX:'45deg'}, {rotateX:'0'})
+
 });
-
-rows.forEach(function (row,i) {
-
-  let rowTL = gsap.timeline({
-    scrollTrigger: {
-      trigger: row,
-      pinSpacing: false,
-      pin: true,
-      start: 'top top',
-      end: "+=" + rows.length * 100 + "%"
-    }
-  })
-});
-

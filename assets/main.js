7b942d71439907a971bce0c9dc1634bf0a423afb
philipp <philipp.wienes@einfach.es>
2022-01-30 20:17:12 +0100

    ay

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,4 @@
-let rows = document.querySelectorAll("h1 div");
+let rows = document.querySelectorAll(".anim");
 let main = document.querySelector("main");
 let totalDuration = main.dataset.animationduration;
 if(totalDuration <= main.offsetHeight) totalDuration = main.offsetHeight;
@@ -34,15 +34,15 @@ rows.forEach(function (row,i) {
 
   var myStartTime = row.dataset.startpercent/100 * totalDuration;
   const myDur = (row.dataset.endpercent - row.dataset.startpercent)/100 * totalDuration;
-  var myOverlap = row.dataset.overlap;
   var myEndTime;
 
-  if(myOverlap){
-    myStartTime = '-=' +  rows[i-1].dataset.startpercent/100 * totalDuration / 2 + '%';
-    myEndTime = '-=' +  ((rows[i-1].dataset.startpercent/100 * totalDuration / 2) - 100) + '%';
-    console.log(myStartTime);
-    console.log(myEndTime);
-  }
+  if(i > 0 && rows[i-1].dataset.startpercent === rows[i].dataset.startpercent && rows[i-1].dataset.endpercent === rows[i].dataset.endpercent)
+    {
+      myStartTime = '-=' +  rows[i-1].dataset.startpercent/100 * totalDuration / 2 + '%';
+      myEndTime = '-=100%';
+      console.log(myStartTime);
+      console.log(myEndTime);
+    }
 
   console.log(myStartTime);
   console.log(myDur);
@@ -52,7 +52,8 @@ rows.forEach(function (row,i) {
     opacity: 0,
   });
 
-  tl.fromTo(row, {autoAlpha: 0}, {autoAlpha:1, duration: myDur / 2}, myStartTime)
-  tl.to(row, {opacity:0, duration: myDur / 2}, myEndTime)
+  tl.fromTo(row, {autoAlpha: 0, y: 40}, {autoAlpha:1, y:0, duration: myDur / 2}, myStartTime)
+  tl.to(row,{}, '+=100')
+  tl.fromTo(row, {y:0, opacity:1}, {opacity:0, y:-20, duration: myDur / 2}, myEndTime)
 
 });
